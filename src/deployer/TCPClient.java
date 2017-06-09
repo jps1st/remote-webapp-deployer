a37ec526a585fda2e123e378e9c80f116bb5a09b
John Solano <johnphilipsolano@gmail.com>
2017-06-10 00:33:54 +0800

    Renamed source files references.

--- src/deployer/TCPClient.java
+++ src/deployer/TCPClient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package smilesdeployer;
+package deployer;
 
 // TCPClient.java
 // A client program implementing TCP socket
--- src/deployer/TCPServer.java
+++ src/deployer/TCPServer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package smilesdeployer;
+package deployer;
 
 // TCPServer.java
 // A server program implementing TCP socket
